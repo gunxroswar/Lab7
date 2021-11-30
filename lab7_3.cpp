eb6dabd762d986fd2b1e242bc5d1b9af075d2ccd
gunxroswar <gxz.gunforce@gmail.com>
2021-11-30 15:40:45 +0700

    "finished lab7-3"

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,30 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
+
+int adiff(int a,int b)
+{
+  while(a>=180 or b>=180)
+  {
+    a = a-360;
+    b = b-360;
+  }
+  int c = abs(a-b);
+  while(c >= 180)
+  {
+    c=c-360;
+  }
+  return abs(c); 
+}
 
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
+  cout << adiff(60,244)<< endl;
 }
